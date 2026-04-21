486e67ae1261e6db5c49dcded8e29cfa23452c38
eventseat21 <eventseat21@users.noreply.github.com>
2026-04-21 02:18:42 +0200

    Fix: Update nodemailer to v7 and add npmrc for Vercel build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pjDFtgrxI5GO9bVL8e8SeK0F8vxsvBIoCW5qnx2HbeA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VlnNEkuQqC8W1ASpQKo2F503WCWfvRhOPEZhmbOBeyA=\"\n}"
