96aab1652f932ef38c0edd6f896d830e06743fcf
eventseat21 <eventseat21@users.noreply.github.com>
2026-04-21 19:24:44 +0200

    Add missing utils.ts with formatPrice function

---
+++ lib/utils.ts
@@ -0,0 +1,7 @@
+export function formatPrice(price: number): string {
+  return new Intl.NumberFormat('tr-TR', {
+    style: 'currency',
+    currency: 'TRY',
+    minimumFractionDigits: 2
+  }).format(price)
+}
