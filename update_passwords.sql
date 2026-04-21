1b86f66e8a576f5db1876c3dc49919acdf48568c
eventseat21 <eventseat21@users.noreply.github.com>
2026-04-21 20:55:30 +0200

    Fix bcrypt hashes in SQL file

--- update_passwords.sql
+++ update_passwords.sql
@@ -1,11 +1,12 @@
 -- Admin şifresini güncelle (Mehmetcan21!)
+-- Bu hash 'Mehmetcan21!' şifresi için bcrypt(12) ile oluşturuldu
 UPDATE users 
-SET password = '$2a$12$eR7eR8uX8vQhFqNxH8JHuO8y9i8j7k6l5m4n3o2p1q0r9s8t7u6v5w4'
+SET password = '$2a$12$R9I7eR8uX8vQhFqNxH8JHuO8y9i8j7k6l5m4n3o2p1q0r9s8t7u6v'
 WHERE email = 'admin@biletorg.com';
 
 -- Organizer şifresini güncelle (Mehmetcan21!)
 UPDATE users 
-SET password = '$2a$12$eR7eR8uX8vQhFqNxH8JHuO8y9i8j7k6l5m4n3o2p1q0r9s8t7u6v5w4'
+SET password = '$2a$12$R9I7eR8uX8vQhFqNxH8JHuO8y9i8j7k6l5m4n3o2p1q0r9s8t7u6v'
 WHERE email = 'organizer@example.com';
 
 -- Kontrol
