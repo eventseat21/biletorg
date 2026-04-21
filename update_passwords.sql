712d9f9b0305e8a8565bfd631b222dcefad23b73
eventseat21 <eventseat21@users.noreply.github.com>
2026-04-21 21:57:42 +0200

    Add correct bcrypt hash for Mehmetcan21! password

--- update_passwords.sql
+++ update_passwords.sql
@@ -1,12 +1,11 @@
 -- Admin şifresini güncelle (Mehmetcan21!)
--- Bu hash 'Mehmetcan21!' şifresi için bcrypt(12) ile oluşturuldu
 UPDATE users 
-SET password = '$2a$12$R9I7eR8uX8vQhFqNxH8JHuO8y9i8j7k6l5m4n3o2p1q0r9s8t7u6v'
+SET password = '$2a$12$/gpuDXKr1/f/uTdPXJ5JxOCWKlAhZ8BmrTardCa9Xq3rSlenIz55e'
 WHERE email = 'admin@biletorg.com';
 
 -- Organizer şifresini güncelle (Mehmetcan21!)
 UPDATE users 
-SET password = '$2a$12$R9I7eR8uX8vQhFqNxH8JHuO8y9i8j7k6l5m4n3o2p1q0r9s8t7u6v'
+SET password = '$2a$12$/gpuDXKr1/f/uTdPXJ5JxOCWKlAhZ8BmrTardCa9Xq3rSlenIz55e'
 WHERE email = 'organizer@example.com';
 
 -- Kontrol
