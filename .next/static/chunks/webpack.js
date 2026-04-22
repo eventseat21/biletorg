6e680050622c65994f69bf3ae6352e0bebcb98cc
eventseat21 <eventseat21@users.noreply.github.com>
2026-04-23 01:51:45 +0200

    Reset auth - new simple login

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0131024091722de9"; }
+/******/ 		__webpack_require__.h = function() { return "47851460502fc00d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
